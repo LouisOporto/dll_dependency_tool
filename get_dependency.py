7b6afad8d5df28631c370de992ad44f9eaf95f32
LouisOporto <louisoporto042@csu.fullerton.edu>
2025-03-16 15:23:34 -0700

    Initial commit

---
+++ get_dependency.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+import shutil
+import re
+
+REGEX = r'([a-zA-Z]:[\\\/](?:[a-zA-Z0-9]+[^WINDOWS][\\\/])+.+\.dll\b)'# (.+\.dll)'
+
+if __name__ == "__main__":
+    executable_path = input("Executable Path: ")
+    output = subprocess.check_output(f"ntldd {executable_path}", shell=True).decode("utf-8")
+
+    file_paths = output.replace('\n', '').replace('\r', ' ').replace('\t','')
+    print(file_paths)
+
+    file_paths = re.findall(REGEX, output)
+    print(file_paths)
+    
+    if not os.path.exists("required_libraries"):
+        os.mkdir("required_libraries")
+
+    build_path = os.getcwd() + "\\required_libraries"
+    for filepath in file_paths:
+        print("Retrieving " + filepath)
+        try:
+            shutil.copy2(filepath, build_path)
+        except PermissionError:
+            print("File already exists at build path")
